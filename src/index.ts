711c407d0bdf6b09f861a105be4cf61c0d558bf8
mjangid-mdsol <mjangid@mdsol.com>
2021-06-22 13:26:10 +0530

    stage all

---
+++ src/index.ts
@@ -0,0 +1,81 @@
+import 'reflect-metadata';
+import { MikroORM } from '@mikro-orm/core';
+import { __prod__ } from './constants';
+// import { Post } from './entities/Post';
+import microConfig from './mikro-orm.config'
+import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+import { buildSchema } from 'type-graphql'
+import { HelloResolver } from './resolvers/hello';
+import { PostResolver } from './resolvers/post';
+import { UserResolver } from './resolvers/user';
+import redis from 'redis';
+import session from 'express-session';
+
+import connectRedis from 'connect-redis'
+import { MyContext } from './types';
+
+const main = async() =>{
+    const orm = await MikroORM.init(microConfig);
+    await orm.getMigrator().up(); 
+
+    const app = express();
+
+    //cookies
+    const RedisStore = connectRedis(session)
+    let redisClient = redis.createClient()
+
+    app.use(
+        session({
+            name:"qid",
+            store: new RedisStore({ 
+                client: redisClient,
+                disableTouch: true
+         }),
+            cookie:{
+                maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years store
+                httpOnly: true,   // dont show to front end
+                secure: __prod__,
+                sameSite: "lax",
+            },
+            // saveUninitialized: false,
+            secret: 'dkjlshadfjkhsdfjksdhfjkdsflksfd',
+            resave: false,
+        })
+        )
+ 
+    
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver, PostResolver, UserResolver],
+            validate: false
+        }),
+        context: ({ req, res }) : MyContext => ({ em: orm.em, req, res })
+    })
+
+    apolloServer.applyMiddleware({ app })
+    // app.get('/',(_,res) =>{
+    //     res.send("hey there")
+    // })
+
+    app.listen(4000,()=>{
+        console.log("server is listening at localhost:4000/graphql")
+    })
+    // const x = new Date()
+    // let dayPost = x.getDate();
+    // const post = orm.em.create(Post, {title: `my ${dayPost}'s post on reddit`})
+    // await orm.em.persistAndFlush(post)
+}
+
+// const find = async() => {
+//     const orm = await MikroORM.init(microConfig);
+//     const post = await orm.em.find(Post, {})
+//     console.log(post)
+// }
+main().catch(err =>{
+    console.error(err)
+})
+
+// find().catch(err =>{
+//     console.error(err)
+// })
